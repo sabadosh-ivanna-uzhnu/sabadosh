27777186c7945a8c682fce814af07645949ee2f9
ivanna sabadosh <sabadosh.ivanna1@student.uzhnu.edu.ua>
2025-02-18 18:06:35 +0200

    add new page

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get("/a", function (req, res, next) {
+  res.send("drink water");
+});
+
 module.exports = router;
